ad8501ae0994a04d3a96fc8f13b853d78468d905
@hexlet/cli <support@hexlet.io>
2023-08-01 12:08:00 +0300

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,42 @@
+package exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+// BEGIN
+public class App {
+    public static LinkedHashMap<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        LinkedHashMap<String, String> result = new LinkedHashMap<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        for (String key : keys) {
+            if (!data1.containsKey(key)) {
+                result.put(key, "added");
+            } else if (!data2.containsKey(key)) {
+                result.put(key, "deleted");
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put(key, "unchanged");
+            } else {
+                result.put(key, "changed");
+            }
+        }
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//        Map<String, Object> data1 = new HashMap<>(
+//                Map.of("one", "eon", "two", "two", "four", true)
+//        );
+//
+//        Map<String, Object> data2 = new HashMap<>(
+//                Map.of("two", "own", "zero", 4, "four", true)
+//        );
+//
+//        System.out.println(genDiff(data1, data2));
+//    }
+}
+//END
